c8ba517b2992fe201a82670196078b04be986fcb
Vinicius Patrinhani <patrinhani@ciandt.com>
2017-10-03 13:58:54 -0300

    tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,12 +3,13 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-abbott:app', () => {
-  beforeEach(function (done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
+describe('generator-abbott:app', function () {
+  this.timeout(15000);
+
+  beforeEach(() => {
+    return helpers.run(path.join(__dirname, '../generators/app'))
     .withPrompts({
-    })
-    .then(() => done());
+    });
   });
 
   it('creates files', () => {
