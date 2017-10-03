4e120e1d85c04ee38f9eb8626567139904b11efa
Vinicius Patrinhani <patrinhani@ciandt.com>
2017-10-03 13:52:00 -0300

    unit tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,11 +4,11 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-abbott:app', () => {
-  before(function () {
-    this.timeout(3000);
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({
-      });
+  beforeEach(function (done) {
+    helpers.run(path.join(__dirname, '../generators/app'))
+    .withPrompts({
+    })
+    .then(() => done());
   });
 
   it('creates files', () => {
